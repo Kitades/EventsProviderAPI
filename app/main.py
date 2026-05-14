ac2851409cb1ecc5b7e948618b2ca89143cb442b
Maksim <maks.severgin80@gmail.com>
2026-05-15 01:13:50 +0700

    main

--- app/main.py
+++ app/main.py
@@ -1,18 +1,8 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 
 @app.get("/api/health")
-async def health():
+def health_check():
     return {"status": "ok"}
-
-# Попробуй ПОКА НЕ ВКЛЮЧАТЬ остальное, чтобы проверить, пройдет ли Health Check
-# app.include_router(router)

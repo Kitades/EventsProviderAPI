b78e2462227b242724bac47a50dab092ed955006
Maksim <maks.severgin80@gmail.com>
2026-05-15 01:03:39 +0700

    main

--- app/main.py
+++ app/main.py
@@ -1,37 +1,18 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from app.routers import router
-from app.database import engine
-from app.models import Base
+from contextlib import asynccontextmanager
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # При старте приложения создаем таблицы, если их нет
-    async with engine.begin() as conn:
-        # Это поможет избежать 503 из-за отсутствия колонок
-        await conn.run_sync(Base.metadata.create_all)
-    try:
-        yield
-    finally:
-        # Тут можно закрывать соединения, если нужно
-        pass
+    yield
 
 
-app = FastAPI(title="Events Aggregator", lifespan=lifespan)
+app = FastAPI(lifespan=lifespan)
 
 
-# Прямой Health Check (тест просит именно /api/health)
 @app.get("/api/health")
-async def health_check():
+async def health():
     return {"status": "ok"}
 
-
-# Подключаем остальные роутеры
-app.include_router(router)
-
-# Запуск для локальной отладки (на LMS не мешает)
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+# Попробуй ПОКА НЕ ВКЛЮЧАТЬ остальное, чтобы проверить, пройдет ли Health Check
+# app.include_router(router)
